de15fa9aeb7b3dda091babc7d5327d1aa5cafa88
Antonio-Alisson-Teixeira-Cruz <AntonioAlissonTeixeiraCruz@gmail.com>
2024-08-16 11:20:02 -0300

    Create script.js

---
+++ Cadastro/script.js
@@ -0,0 +1,8 @@
+let nome = prompt('Qual é o seu nome?')
+let sobrenome = prompt('Qual é o seu sobrenome?')
+let anoNascimento = prompt('Qual o ano que você nasceu?') 
+let cursoTecnico = prompt('Qual curso técnico você está cursando?')
+
+let idade = 2024 - anoNascimento
+
+alert('Olá ' + nome + ' ' + sobrenome + '\n' + ' então você estuda ' + cursoTecnico + ' e tem ' + idade + ' anos. Legal!')
